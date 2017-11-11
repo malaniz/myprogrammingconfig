98eca294b6e906a64771459d2e48b3e21f3aab90
Marcelo Alaniz <alanizmarcelo@gmail.com>
2017-11-11 03:30:35 -0300

    installing scripts

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+ln -s $HOME/dev/configs/nvim $HOME/.config
+ln -s $HOME/dev/configs/tmux $HOME/.config
+ln -s $HOME/dev/configs/synergy $HOME/.config
+ln -s $HOME/dev/configs/termine $HOME/.config
+ln -s $HOME/dev/configs/spectrwm/spectrwm.conf $HOME/.spectrwm.conf
+ln -s $HOME/dev/configs/spectrwm/spectrwm_es.conf $HOME/.spectrwm_es.conf
+
