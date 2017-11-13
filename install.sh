3635f63ea4445af51c4b4e3b6c1351c867d3ee37
Marcelo Alaniz <alanizmarcelo@gmail.com>
2017-11-13 12:01:30 -0300

    fixing install and aliasses

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-ln -s $HOME/dev/configs/nvim $HOME/.config
-ln -s $HOME/dev/configs/tmux $HOME/.config
-ln -s $HOME/dev/configs/synergy $HOME/.config
-ln -s $HOME/dev/configs/termine $HOME/.config
-ln -s $HOME/dev/configs/spectrwm/spectrwm.conf $HOME/.spectrwm.conf
-ln -s $HOME/dev/configs/spectrwm/spectrwm_es.conf $HOME/.spectrwm_es.conf
+ln -s $PWD/nvim $HOME/.config
+ln -s $PWD/tmux $HOME/.config
+ln -s $PWD/synergy $HOME/.config
+ln -s $PWD/termite $HOME/.config
+ln -s $PWD/spectrwm/spectrwm.conf $HOME/.spectrwm.conf
+ln -s $PWD/spectrwm/spectrwm_es.conf $HOME/.spectrwm_es.conf
+ln -s $PWD/terminal/aliases $HOME/.aliases
 
