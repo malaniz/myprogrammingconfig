6d94e5ae64b1a342da3e834a80ad65b333d23f50
Marcelo Alaniz <alanizmarcelo@gmail.com>
2018-03-06 04:57:22 -0300

    adding personal scripts && ninja keyboard config

--- install.sh
+++ install.sh
@@ -7,4 +7,7 @@ ln -s $PWD/termite $HOME/.config
 ln -s $PWD/spectrwm/spectrwm.conf $HOME/.spectrwm.conf
 ln -s $PWD/spectrwm/spectrwm_es.conf $HOME/.spectrwm_es.conf
 ln -s $PWD/terminal/aliases $HOME/.aliases
+ln -s $PWD/ninjakb $HOME/.ninjakb
+ln -s $PWD/commands $HOME/bin
+
 
