726f762f6120f03d0c17a42653723562a9611394
Marcelo Alaniz <alanizmarcelo@gmail.com>
2019-03-20 12:53:43 -0300

    dunst

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ln -s $PWD/tmux $HOME/.config
 ln -s $PWD/synergy $HOME/.config
 ln -s $PWD/termite $HOME/.config
 ln -s $PWD/kitty $HOME/.config
+ln -s $PWD/dunst $HOME/.config
 ln -s $PWD/spectrwm/spectrwm.conf $HOME/.spectrwm.conf
 ln -s $PWD/spectrwm/spectrwm_es.conf $HOME/.spectrwm_es.conf
 ln -s $PWD/terminal/aliases $HOME/.aliases
