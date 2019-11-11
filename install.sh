962d55ded34a86ea017e7a04e5e28d4a84df0d19
Marcelo Alaniz <alanizmarcelo@gmail.com>
2019-11-11 09:51:53 -0300

    last changes

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ ORIG=$PWD/synergy                   && LINK=$HOME/.config/synergy   && installit
 ORIG=$PWD/termite                   && LINK=$HOME/.config/termite   && installit $ORIG $LINK
 ORIG=$PWD/kitty                     && LINK=$HOME/.config/kitty     && installit $ORIG $LINK
 ORIG=$PWD/dunst                     && LINK=$HOME/.config/dunst     && installit $ORIG $LINK
+ORIG=$PWD/rofi                      && LINK=$HOME/.config/rofi      && installit $ORIG $LINK
 ORIG=$PWD/spectrwm/spectrwm.conf    && LINK=$HOME/.spectrwm.conf    && installit $ORIG $LINK
 ORIG=$PWD/spectrwm/spectrwm_es.conf && LINK=$HOME/.spectrwm_es.conf && installit $ORIG $LINK
 ORIG=$PWD/terminal/aliases          && LINK=$HOME/.aliases          && installit $ORIG $LINK
